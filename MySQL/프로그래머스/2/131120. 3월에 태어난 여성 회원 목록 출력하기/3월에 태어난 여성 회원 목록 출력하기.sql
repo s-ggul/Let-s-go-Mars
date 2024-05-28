d4a28e3406e369ae66ce34a2655e6eb510391a18
Sunghan Kim <80890062+s-ggul@users.noreply.github.com>
2024-05-28 21:19:42 +0900

    Delete MySQL/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기 directory

--- MySQL/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++
@@ -1,9 +0,0 @@
-# IS NULL, IS NOT NULL, IF NULL 관련 참고 사이트 
-# https://ssafy-story.tistory.com/64
-
-    SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, "%Y-%m-%d") as 'DATE_OF_BIRTH'
-      FROM MEMBER_PROFILE
-     WHERE MONTH(DATE_OF_BIRTH) = 3
-       AND TLNO IS NOT NULL
-       AND GENDER = 'W'
-  ORDER BY MEMBER_ID asc;
