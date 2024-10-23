28be30436a827839fca71ae574cb1d47687361ac
Sunghan Kim <80890062+s-ggul@users.noreply.github.com>
2024-10-23 22:00:48 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int angle) {
+        if(angle == 180) return 4;
+        else if(angle > 90) return 3;
+        else if(angle == 90) return 2;
+        return 1;
+    }
+}
